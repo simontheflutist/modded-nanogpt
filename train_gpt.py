bf4bad712d72d57b701513ca58606a26869886af
Simon Kuang <simontheflutist@berkeley.edu>
2025-12-21 16:10:40 -0800

    smaller wd in normuon

--- train_gpt.py
+++ train_gpt.py
@@ -1472,7 +1472,7 @@ optimizer1 = DistAdam(
     eps=1e-8,
     weight_decay=0.003,
 )
-optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.023, momentum=0.95, beta2=0.95, weight_decay=1.2)
+optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.023, momentum=0.95, beta2=0.95, weight_decay=1.2 * 0.1)
 optimizers = [optimizer1, optimizer2]
 for opt in optimizers:
     for group in opt.param_groups:
